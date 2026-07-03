a444678b79049a1d015219384a0c697eb0e4e0d8
Xxlilsolid <127943695+Xxlilsolid@users.noreply.github.com>
2026-07-04 00:54:38 +0100

    [settings] Temporary workaround where moving the frozen binaries from the cloned repo breaks the app

--- settings.py
+++ settings.py
@@ -6,4 +6,7 @@ finishedLoading = False
 toggleplay = 0
 desktop_session = ''
 LOGFILE = './logs/latest.log'
-APP_VER = str(subprocess.check_output(["git", "rev-parse", "--short", 'HEAD']).strip())[2:-1]
+try: 
+    APP_VER = str(subprocess.check_output(["git", "rev-parse", "--short", 'HEAD']).strip())[2:-1]
+except Exception as e:
+    APP_VER = "Frozen"
